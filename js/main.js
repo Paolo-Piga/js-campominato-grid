c58e7c13fe987a50a92b00c4313bd4dbb01f1b4a
DecimTheJudge <paolo1piga@gmail.com>
2024-06-13 17:38:14 +0200

    continuo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 // creo un bottone per iniziare il gioco
 const inizio = document.querySelector(".inizio");
 inizio.addEventlistener("click"); 
+
 // il bottone dovrà generare una griglia e le 100 caselle, 10 per riga e 10 per colonna
+const griglia = createElementWithClass("div");
+
+for (let i = 1; i <= 100; i++) {
+    const quadrato = createElementWithClass("div");
+}
 
 // quando la casella verrà cliccata essa cambierà di colore
+quadrato = addEventListener("click");
 
