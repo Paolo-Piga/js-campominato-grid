6801e4da4e002fdbe1f757dc6c3b2157b2a254ee
DecimTheJudge <paolo1piga@gmail.com>
2024-06-13 17:32:17 +0200

    aggiunta dei commenti

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// creo un bottone per iniziare il gioco
+
+// il bottone dovrà generare una griglia e le 100 caselle, 10 per riga e 10 per colonna
+
+// quando la casella verrà cliccata essa cambierà di colore
+
