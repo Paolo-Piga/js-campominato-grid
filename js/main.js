474e3db1909002e700c2c42e0875931974937ea8
DecimTheJudge <paolo1piga@gmail.com>
2024-06-13 17:34:48 +0200

    creazione bottone

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // creo un bottone per iniziare il gioco
-
+const inizio = document.querySelector(".inizio");
+inizio.addEventlistener("click"); 
 // il bottone dovrà generare una griglia e le 100 caselle, 10 per riga e 10 per colonna
 
 // quando la casella verrà cliccata essa cambierà di colore
